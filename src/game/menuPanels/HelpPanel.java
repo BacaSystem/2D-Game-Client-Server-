f3d598561f14538a0ce14fa83a708362b0398d92
maciekx7 <maciek.k1999@gmail.com>
2020-04-03 15:09:11 +0200

    LavaDoc upgrade

--- src/game/menuPanels/HelpPanel.java
+++ src/game/menuPanels/HelpPanel.java
@@ -8,9 +8,16 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 
+/**
+ * Klasa zajmujaca się wyświetlaniem widoku Help Menu
+ * Rozszerza klasę JPanel
+ */
 public class HelpPanel extends JPanel {
-
-    public HelpPanel(int panelWidth, int panelHeight, ActionListener menuListner) {
+    /**
+     * Konstruktor tworzący komponenty do wyświetlenia w panelu pomocy
+     * @param menuListner Listner z menu nasluchujący wciśnięcia przycisków
+     */
+    public HelpPanel(ActionListener menuListner) {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(new EmptyBorder(5, 6, 5, 6));
 
@@ -20,7 +27,6 @@ public class HelpPanel extends JPanel {
         JPanel buttonPanel = new JPanel(new GridLayout(10, 1, 10, 5));
 
         //HERE add components
-        //buttonPanel.add(new JLabel("HELP PAGE", SwingConstants.CENTER));
         buttonPanel.add(new JLabel(getHelp("helpText", "tytul"), SwingConstants.CENTER));
         JTextArea help = new JTextArea(getHelp("helptext", "punkt"));
         help.setEditable(false);
@@ -34,6 +40,12 @@ public class HelpPanel extends JPanel {
         this.add(panel);
     }
 
+    /**
+     * Metoda pobierająca tekst pomocy
+     * @param fileName nazwa pliku pomocy
+     * @param key klucz property
+     * @return zwraca wartość spod klucza
+     */
     private String getHelp(String fileName, String key) {
         return GetConfigProperties.getValue(fileName, key);
     }
--- src/game/menuPanels/MenuPanel.java
+++ src/game/menuPanels/MenuPanel.java
@@ -8,13 +8,27 @@ import java.awt.event.ActionListener;
 import game.Constant.MenuWindowStates;
 import game.entities.Button;
 
+
+/**
+ * Klasa zajmujaca się wyświetlaniem widoku Menu głównego
+ * Rozszerza klasę JPanel
+ */
 public class MenuPanel extends JPanel {
 
+    /** Lista nazw przycisków w menu */
     private String[] buttonNames = {MenuWindowStates.NEW_GAME_BUTTON, MenuWindowStates.HIGH_SCORES_BUTTON, MenuWindowStates.HELP_BUTTON, MenuWindowStates.EXIT_BUTTON};
+    /** Lista nazw akcji wywolywanych przez pryciski*/
     private String[] buttonActionNames = {MenuWindowStates.NEW_GAME, MenuWindowStates.HIGH_SCORES, MenuWindowStates.HELP, MenuWindowStates.EXIT};
+    /** Liczba przycisków */
     private int howManyButtons = buttonActionNames.length;
 
-    public MenuPanel(int panelWidth, int panelHeight, ActionListener menuListner) {
+    /**
+     * Konstruktor tworzący komponenty do wyświetlenia w głównym menu
+     * @param menuListner Listner nasłuchujący wciśnięcia przycisków
+     * Przyciski tworzone są dynamicznie w zależności od dlugości tablicy przycisków
+     * Przyciski tworzone są z pomocą klasy Button
+     */
+    public MenuPanel(ActionListener menuListner) {
         JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(new EmptyBorder(5, 6, 5, 6));
 
@@ -22,6 +36,7 @@ public class MenuPanel extends JPanel {
         layout.setBorder(new EmptyBorder(10, 10, 10, 10));
 
         JPanel buttonPanel = new JPanel(new GridLayout(10, 1, 10, 5));
+
         buttonPanel.add(new JLabel("LANDER", SwingConstants.CENTER));
         for (var i = 0; i < howManyButtons; i++) {
             buttonPanel.add(new Button(menuListner, buttonNames[i], buttonActionNames[i]));
@@ -31,10 +46,6 @@ public class MenuPanel extends JPanel {
         panel.add(layout, BorderLayout.CENTER);
         setLayout(new GridLayout(5, 1, 20, 20));
         this.add(panel);
-
-
-        //setPreferredSize(new Dimension(panelWidth, panelHeight));
-
 
         this.add(panel);
     }
