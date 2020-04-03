35f5e92b0ec71f06c6ec3c23d79d084ba0b5737f
Dominik Baczyński <dombacy@gmail.com>
2020-04-03 19:27:35 +0200

    Meteor scratch

---
+++ src/game/entities/Meteor.java
@@ -0,0 +1,26 @@
+package game.entities;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class Meteor {
+
+    private Rectangle2D meteor;
+
+    public Meteor(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void render(Graphics2D g){
+
+    }
+
+    public Rectangle2D getCollider(){
+        return new Rectangle2D.Float();
+    }
+
+}
