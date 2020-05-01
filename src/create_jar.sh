7d1d5d9fef02cd0a80c9f989b699681c84944535
maciekx7 <maciek.k1999@gmail.com>
2020-05-01 22:38:10 +0200

    jar files

--- src/create_jar.sh
+++ src/create_jar.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 jar cvfm Game.jar game/META-INF/MANIFEST.MF game/*.class game/physic/*.class game/Constant/*.class game/controller/*.class game/data/*.class game/launcher/*.class game/view/*.class game/window/*.class game/entities/*.class game/configReader/*.class game/serverConnection/*.class game/interfaces/*.class game/manager/*.class
+
+jar cvfm Server.jar server/META-INF/MANIFEST.MF server/configReader/*.class server/*.class
+
